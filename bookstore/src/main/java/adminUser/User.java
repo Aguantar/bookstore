32f0685869ad8d8007d17156f8e52dc1907837da
wonbinown <12wonbin1@gmail.com>
2025-07-16 18:11:37 +0900

    Add files via upload

---
+++ bookstore/src/main/java/adminUser/User.java
@@ -0,0 +1,13 @@
+package adminUser;
+
+import lombok.Data;
+
+@Data
+public class User {
+    private Long id;
+    private String name;
+    private String password;
+    private String email;
+    private String hp;
+    private String role;
+}
